18ce1bab9f201ede8de3c4e7e7d3a21ee1a2e7f1
mattstilling <mjstilling@gmail.com>
2022-01-21 16:22:32 -0600

    Created a basic GUI layout for the application
    Added two scenes to GeneratorApp.java:
    (1) A start scene where the user generates a movie
    (2) A movie info scene where the user is shown info for their newly generated movie
    Included the appropriate buttons and labels to guide the user between the two scenes.

--- MovieGenerator/src/GeneratorApp.java
+++ MovieGenerator/src/GeneratorApp.java
@@ -1,7 +1,12 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 /**
  * This class runs the movie generator app using JavaFX.
@@ -9,13 +14,53 @@ import javafx.scene.layout.BorderPane;
  * @author Matt Stilling
  */
 public class GeneratorApp extends Application {
+	
+	Scene startScene, movieScene;
 
-	public void start(final Stage stage) {
-		stage.setTitle("Movie Generator");
-		BorderPane borderPane = new BorderPane();
-		Scene scene = new Scene(borderPane, 750, 500);
-		stage.setScene(scene);
-		stage.show();
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Movie Generator");
+		
+		// Starting Scene
+		BorderPane startLayout = new BorderPane();
+		startScene = new Scene(startLayout, 750, 500);
+		
+		Label welcomeLabel = new Label("Welcome to Movie Generator");
+		startLayout.setTop(welcomeLabel);
+		BorderPane.setAlignment(welcomeLabel, Pos.TOP_CENTER);
+		
+		Button generateButton = new Button("Generate Movie");
+		generateButton.setOnAction(event -> primaryStage.setScene(movieScene));
+		startLayout.setCenter(generateButton);
+		
+		Button exitButton1 = new Button("Exit");
+		exitButton1.setOnAction(event -> Platform.exit());
+		startLayout.setBottom(exitButton1);
+		BorderPane.setAlignment(exitButton1, Pos.BOTTOM_RIGHT);
+		
+		// Movie Scene (shows movie info after movie is generated)
+		BorderPane movieLayout = new BorderPane();
+		
+		Label generatedMovieLabel = new Label("Generated Movie: TBA");
+		movieLayout.setTop(generatedMovieLabel);
+		BorderPane.setAlignment(generatedMovieLabel, Pos.TOP_CENTER);
+		
+		VBox movieInfo = new VBox(5);
+		Label movieLabel = new Label("This will contain movie info");
+		Button returnButton = new Button("Return to Generate Page");
+		returnButton.setOnAction(event -> primaryStage.setScene(startScene));
+		movieInfo.getChildren().addAll(movieLabel, returnButton);
+		movieLayout.setCenter(movieInfo);
+		
+		Button exitButton2 = new Button("Exit");
+		exitButton2.setOnAction(event -> Platform.exit());
+		movieLayout.setBottom(exitButton2);
+		BorderPane.setAlignment(exitButton2, Pos.BOTTOM_RIGHT);
+		
+		movieScene = new Scene(movieLayout, 750, 500);
+		
+		primaryStage.setScene(startScene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
